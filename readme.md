20448f3c1f221511dfb10e751f2cf840f5d38a34
Roger Orellana Castells <roy.ore@gmail.com>
2022-12-09 01:07:24 +0100

    Fix try catch technologies

--- readme.md
+++ readme.md
@@ -1,3 +1,24 @@
 # Prisma
 
-npx prisma generate
+Update prisma file
+
+```npx prisma generate```
+
+# Deploy 
+
+Pull files
+
+```git pull```
+
+Update prisma file
+
+```npx prisma generate```
+
+Generate dist files
+
+```npm run tsc```
+
+Deploy in Google Cloud, from folder apps:
+
+```sh node_start.sh ```
+
--- src/v1/routes/technologies.ts
+++ src/v1/routes/technologies.ts
@@ -1,11 +1,16 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import * as techService from '../../services/technologyService';
 
 const router = express.Router();
 
-router.get('', async (_, res) => {
-    const techs = await techService.getEntries();
-    res.send(techs);
+router.get('', async (_req: Request, res: Response, _next: NextFunction)  => {
+    try {
+        const techs = await techService.getEntries();
+        res.send(techs);
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/:id', async (req, res) => {
